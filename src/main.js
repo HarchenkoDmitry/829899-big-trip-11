1224c2db19e0b890e2a0635b539fb0db96e6f9eb
Дмитрий <harchenkodmitrii@bk.ru>
2020-04-02 21:53:14 +0300

    added loop events

--- src/main.js
+++ src/main.js
@@ -98,12 +98,19 @@ const createSortTemplate = () => {
 };
 
 const createEventsListTemplate = () => {
+  const AMOUNT_EVENTS = 3;
+  const events = [];
+
+  for (let i = 0; i < AMOUNT_EVENTS; i++) {
+    events.push(createEventTemplate());
+  }
+
   return (
     `<ul class="trip-events__list">
       ${createEventEditTemplate()}
-      ${createEventTemplate()}
-      ${createEventTemplate()}
-      ${createEventTemplate()}
+      
+      ${events.join(``)}
+
     </ul>`
   );
 };
